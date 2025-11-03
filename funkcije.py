d074082d15773d7d99fa11b6793218ca19384044
demiribrisimovic33-wq <demiribrisimovic33@gmail.com>
2025-11-03 09:11:46 +0000

    funkcije

---
+++ funkcije.py
@@ -0,0 +1,34 @@
+# funkcije
+
+# def ime_funkcije():
+def pozdravi():
+    print("hello world")
+
+def pozdraviime(ime):
+        print(f"pozrdavljen {ime}")
+# {} altgr + b/n
+
+def pozdraviimepriimek(ime, primek): 
+    print("Pozdravljen {ime} {priimek}")
+
+#pozdravi()
+#pozdraviime("Albatros")
+pozdraviimepriimek("Luka", "Colarič")
+
+
+
+# napiši funkcijo sestej(a, b), ki "printa" seštevek a in b
+# napiši funkcijo zmnozi(a, b); ki "printa" zmnozek a in b
+
+
+def  sestej(a, b):
+     print(a + b)
+
+sestej(10, 20)
+
+
+
+def  zmnozi(a, b):
+     print(a * b)
+
+zmnozi(5, 20)
